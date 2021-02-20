f41009d887ac6edde9406e89f657a0d63881a60d
Conal Walsh <conal.walsh1@gmail.com>
2021-02-20 18:40:05 +0000

    Added pagination views and functionality

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,9 +1,16 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
+import PaginationView from './views/paginationView.js';
 
 import 'regenerator-runtime/runtime';
 import 'core-js/stable';
+import paginationView from './views/paginationView.js';
+
+// if (module.hot) {
+//   module.hot.accept();
+// }
 
 const controlRecipes = async function () {
   try {
@@ -25,6 +32,8 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
+
     // 1) Get search query
     const query = searchView.getQuery();
     if (!query) return;
@@ -33,16 +42,28 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     // 3) Render results
-    console.log(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage());
+
+    // 4) Render pagination btns
+    paginationView.render(model.state.search);
   } catch (err) {
     console.log(err);
   }
+};
+
+const controlPagination = function (goToPage) {
+  // 1) Render new results
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  // 2) Render new pagination btns
+  paginationView.render(model.state.search);
 };
 
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   // Add a handler to the search event in the view and pass the function to call
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
 };
 
 init();
--- src/js/model.js
+++ src/js/model.js
@@ -1,6 +1,7 @@
 import 'regenerator-runtime/runtime';
 import { API_URL } from './config.js';
 import { API_KEY } from './config.js';
+import { RES_PER_PAGE } from './config.js';
 import { getJSON } from './helpers.js';
 
 // https://forkify-api.herokuapp.com/v2
@@ -10,6 +11,8 @@ export const state = {
   search: {
     query: '',
     results: [],
+    page: 1,
+    resultsPerPage: RES_PER_PAGE,
   },
 };
 
@@ -51,4 +54,13 @@ export const loadSearchResults = async function (query) {
   } catch (err) {
     throw err;
   }
+};
+
+export const getSearchResultsPage = function (page = state.search.page) {
+  state.search.page = page;
+
+  const start = (page - 1) * state.search.resultsPerPage;
+  const end = page * state.search.resultsPerPage;
+
+  return state.search.results.slice(start, end);
 };
